51b028ae5255e415a6f699ebd02f30a7caa8e53b
Tat <06805645@dwc-legazpi.edu>
2022-11-16 08:16:13 +0800

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { SignInUpComponent } from './sign-in-up/sign-in-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutUsComponent,
+    ContactsComponent,
+    SignInUpComponent
   ],
   imports: [
     BrowserModule,
