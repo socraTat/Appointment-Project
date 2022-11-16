4cfaf2944ea15d65f0164b6f81745a50e0e0351f
Tat <06805645@dwc-legazpi.edu>
2022-11-16 08:32:32 +0800

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { SignInUpComponent } from './sign-in-up/sign-in-up.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -18,7 +23,11 @@ import { SignInUpComponent } from './sign-in-up/sign-in-up.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'appointment-project-64dbd',
+    appId: '1:430958181897:web:c76a98e21ba2b0f38b02d8',
+    databaseURL: 'https://appointment-project-64dbd-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'appointment-project-64dbd.appspot.com',
+    apiKey: 'AIzaSyAd75ig7pM-LcZdovJlUuTZwTmxL9y9RhE',
+    authDomain: 'appointment-project-64dbd.firebaseapp.com',
+    messagingSenderId: '430958181897',
+  },
   production: true
 };
