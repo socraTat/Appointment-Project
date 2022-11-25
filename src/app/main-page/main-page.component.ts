a296115d6e8766bcb24f7813f93aba5bbc4c0869
Tat <06805645@dwc-legazpi.edu>
2022-11-26 00:28:25 +0800

    added sign up form with firebase authentication ps.kulang pa dis ng guard but ye goods na dis for now

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authServ : AuthenticationService, private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  signOut(){
+    this.authServ.signOut().subscribe(() => {
+      this.route.navigate(['/'])
+    })
   }
 
 }
