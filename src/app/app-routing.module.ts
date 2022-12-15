a271dcbec0f8c6eaae42c1535253b4930e95a72b
Mutya-Ayeng <07007196@dwc-legazpi.edu>
2022-12-16 01:06:29 +0800

    changes sa about us html and css, add component  logout and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { GetAppointmentComponent } from './get-appointment/get-appointment.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LogoutComponent } from './logout/logout.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { MyAppointmentComponent } from './my-appointment/my-appointment.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+
 
 
 const routes: Routes = [
@@ -20,7 +22,8 @@ const routes: Routes = [
   {path: 'main-page', component:MainPageComponent},
   {path: 'user-profile', component:UserProfileComponent},
   {path: 'get-appointment', component:GetAppointmentComponent},
-  {path: 'my-appointment', component:MyAppointmentComponent}
+  {path: 'my-appointment', component:MyAppointmentComponent},
+  {path:'logout', component:LogoutComponent}
  
 ];
 
